83c39165856d4e5557fa964a4a56d5fa486b5bc7
cyj <chenyijie16@126.com>
2018-10-25 00:05:48 +0800

    add some control

--- MusicPlayerASM/main.asm
+++ MusicPlayerASM/main.asm
@@ -16,6 +16,7 @@ include shell32.inc
 include oleaut32.inc
 include msvcrt.inc
 include winmm.inc
+include shfolder.inc
 ;include C:\masm32\macros\macros.asm
 
 ;     libraries
@@ -35,8 +36,15 @@ InitInstance proto:HINSTANCE,:DWORD
 
 
 
-
 .data
+FileInfo struct
+	filename WCHAR 256 DUP(?)
+	filesize LARGE_INTEGER <>
+FileInfo ends
+
+Files FileInfo 1024 DUP(<>)
+
+
 szWindowClass db "MusicPlayer",0
 szTitle db "MusicPlayer",0
 AppName db "MusicPlayer", 0
@@ -44,16 +52,28 @@ szTextFileName db "FileName",0
 szTextFileSize db "FileSize",0
 
 fontname db 'C', 0, 'o', 0, 'n', 0, 's', 0, 'o', 0, 'l', 0, 'a', 0, 's', 0, 0, 0 ; Consolas
-text_button db "button",0
-text_play db "play",0
-text_opendir db "Open Directory",0
+text_button db "button", 0
+text_play db "play", 0
+text_edit db "edit",0
+text_opendir db "Open Directory", 0
+text_browse_folder db "Browse folder", 0
+
+
+
+find_dir_fmt db '%', 0, 's', 0, '\', '*',0, 0, 0; "%s\\*"
+s_fmt db '%',0,'s',0, 0, 0 ;"%s"
+d_fmt db '%', 0, 'd', 0, 0, 0
 PROGRESSCLASS db "msctls_progress32",0
 LISTVIEWCLASS db "SysListView32",0
+
+text_static db "static",0
+text_0000 db "00:00",0
+
 ; 控件的全局ID
 IDC_PLAYBTN HMENU 205
 IDC_LISTVIEW HMENU 206
 IDC_OPENDIRBTN HMENU 207
-IDC_EDITCHILD HMENU 208
+IDC_EDIT HMENU 208
 IDC_CURRENTPLAYTIME HMENU 209
 IDC_TOTALPLAYTIME HMENU 210
 IDC_STOPBTN HMENU 211
@@ -74,7 +94,7 @@ hStopBtn HWND ?
 hTotalPlayTime HWND ?
 hCurrentPlayTime HWND ?
 
-DIR WCHAR 1024 DUP(?)
+DIR WCHAR 1024 DUP(0)
 Info WCHAR 1024 DUP(?)
 
 
@@ -217,6 +237,38 @@ CreateWindowControl proc uses eax hWnd:HWND
 		0, 0, 0, 0,
 		hWnd, IDC_OPENDIRBTN, hInstance, NULL
 	mov hOpenDirBtn, eax
+	invoke SendMessage, hOpenDirBtn, WM_SETFONT, hFont, NULL
+
+
+
+	; 创建编辑框
+	invoke CreateWindowEx, 0, offset text_edit, NULL,
+		WS_CHILD or WS_VISIBLE or ES_LEFT or ES_MULTILINE or \
+		ES_AUTOVSCROLL or ES_READONLY,
+		0, 0, 0, 0, 
+		hWnd,IDC_EDIT,hInstance, NULL
+	mov hwndEdit, eax
+	invoke SendMessage, hwndEdit, WM_SETFONT, hFont, NULL
+
+	; 创建播放时间文本框
+
+	invoke CreateWindowEx, 0, offset text_static, offset text_0000,
+		WS_CHILD or WS_VISIBLE or SS_RIGHT,
+		0, 0, 0, 0,
+		hWnd, IDC_CURRENTPLAYTIME, hInstance, NULL
+	mov hCurrentPlayTime, eax
+	invoke SendMessage, hCurrentPlayTime, WM_SETFONT, hFont, NULL
+
+	invoke CreateWindowEx, 0, offset text_static, offset text_0000,
+		WS_CHILD or WS_VISIBLE or SS_LEFT,
+		0, 0, 0, 0,
+		hWnd, IDC_TOTALPLAYTIME, hInstance, NULL
+	mov hTotalPlayTime, eax
+	invoke SendMessage, hTotalPlayTime, WM_SETFONT, hFont, NULL
+
+
+
+
 	ret
 CreateWindowControl endp
 
@@ -229,7 +281,7 @@ ReSizeWindowControl proc uses eax ebx hWnd:HWND
 	local current_width:DWORD
 	local current_height:DWORD
 	local middle:DWORD
-	local _bottom
+	local playBtn_bottom:DWORD
 
 	local pb_left:DWORD
 	local pb_bottom:DWORD
@@ -240,6 +292,9 @@ ReSizeWindowControl proc uses eax ebx hWnd:HWND
 	local lv_width:DWORD
 	local lv_height:DWORD
 
+	local pl_time_left:DWORD
+
+	local edit_top:DWORD
 
 	; 确定 play 按钮的位置
 	mov eax, 0 
@@ -258,11 +313,11 @@ ReSizeWindowControl proc uses eax ebx hWnd:HWND
 	sub ebx, rc.top
 	mov current_height, ebx
 	mov eax, rc.bottom
-	mov _bottom, eax
-	sub _bottom, 40
+	mov playBtn_bottom, eax
+	sub playBtn_bottom, 40
 
 
-	invoke MoveWindow, hPlayBtn, middle, _bottom, 60, 30, TRUE
+	invoke MoveWindow, hPlayBtn, middle, playBtn_bottom, 60, 30, TRUE
 
 	; 确定进度条的位置
 	mov eax, rc.left
@@ -292,19 +347,173 @@ ReSizeWindowControl proc uses eax ebx hWnd:HWND
 	mov lv_height, eax
 
 	invoke MoveWindow, hListView, lv_left, lv_top, lv_width, lv_height, TRUE
+
+	; 确定打开文件夹按钮位置
+
+	invoke MoveWindow, hOpenDirBtn, lv_left, playBtn_bottom, 120, 30, TRUE
+
+	; 确定播放时间显示位置
+
+	invoke MoveWindow, hCurrentPlayTime, lv_left, pb_bottom, 55, 15, TRUE
+
+	mov eax, rc.right
+	sub eax, 75
+	mov pl_time_left, eax
+	invoke MoveWindow, hTotalPlayTime, pl_time_left, pb_bottom, 55, 15, TRUE
+
+
+	mov eax, rc.bottom
+	sub eax, 200
+	mov edit_top, eax
+	invoke MoveWindow, hwndEdit, lv_left, edit_top, lv_width, 60, TRUE
+
 	ret
 ReSizeWindowControl endp
+
+;------------------------------
+; 选择目录
+;
+SelectDir proc hWnd:HWND
+	local bInfo: BROWSEINFO
+	local lpDlist: DWORD
+	invoke RtlZeroMemory, addr bInfo, sizeof BROWSEINFO ;一定要把内存清0，否则会报错
+	invoke GetForegroundWindow
+	mov bInfo.hwndOwner, eax
+	mov bInfo.lpszTitle, offset text_browse_folder
+	mov bInfo.ulFlags, BIF_RETURNONLYFSDIRS or BIF_USENEWUI or BIF_UAHINT or BIF_NONEWFOLDERBUTTON ;/*不带新建文件夹按钮*/
+	invoke SHBrowseForFolder, addr bInfo
+	mov lpDlist, eax
+
+	cmp eax, 0 ; if lpDlist != NULL
+	;jnz returnselectdir
+	invoke SHGetPathFromIDList ,lpDlist, offset DIR
+returnselectdir:	
+	ret
+SelectDir endp
+
+
+
+FindFile proc
+	local ffd: WIN32_FIND_DATA
+	local hFind: HANDLE
+	local FileCount: DWORD
+	local lvI:LVITEMW
+	local tempdir[260]:WCHAR
+	invoke RtlZeroMemory, addr lvI, sizeof LVITEMW
+	invoke RtlZeroMemory,addr ffd,sizeof WIN32_FIND_DATA
+	mov lvI.pszText, LPSTR_TEXTCALLBACKW
+	mov lvI._mask, LVIF_TEXT or LVIF_IMAGE or LVIF_STATE
+	invoke wsprintfW, addr tempdir, offset find_dir_fmt, offset DIR
+
+	invoke FindFirstFile, addr tempdir, addr ffd
+	mov hFind, eax
+	.if eax == -1
+		ret ; error!
+	.endif
+	mov FileCount, 0
+
+	invoke SendMessage, hwndEdit, WM_SETTEXT, 0, addr tempdir
+
+	do_start:
+		mov eax, ffd.dwFileAttributes
+		and eax, FILE_ATTRIBUTE_DIRECTORY
+		cmp eax, 0
+		jnz do; if it is directory, jump to label 'do'
+
+		
+		mov eax, FileCount
+		mov ebx, sizeof FileInfo
+		mul ebx
+		assume ebx:PTR FileInfo
+		mov ebx, offset Files
+		add ebx, eax
+		invoke wsprintfW, addr [ebx].filename, offset s_fmt, addr ffd.cFileName
+
+		mov eax, ffd.nFileSizeHigh
+		mov [ebx].filesize.HighPart, eax
+		mov eax, ffd.nFileSizeLow
+		mov [ebx].filesize.LowPart, eax
+		assume ebx:nothing
+		inc FileCount
+	do:
+		invoke FindNextFileW, hFind, addr ffd
+		cmp eax, 0
+		jnz do_start
+
+
+
+	invoke SendMessage, hListView, LVM_DELETEALLITEMS, 0, 0
+	mov ecx, 0
+	
+	.while ecx < FileCount
+		inc lvI.iItem
+		push lvI.iItem
+		pop lvI.lParam
+		inc ecx
+		push ecx
+		invoke SendMessage, hListView, LVM_INSERTITEM, 0, addr lvI
+		pop ecx
+	.endw
+
+	ret
+FindFile endp
+
+handleListViewNotify proc lParam:LPARAM
+	mov edx, lParam
+	assume edx:PTR NMHDR
+		; TODO:
+		; 下面的代码还有bug 希望参照解决
+		.if [edx].code == LVN_GETDISPINFO
+			assume edx:PTR NMLVDISPINFOW
+			mov ebx, offset Files
+			mov eax, sizeof FileInfo
+			mul [edx].item.iItem
+			add ebx, eax
+			assume ebx:PTR FileInfo
+			.if [edx].item.iSubItem == 0
+				lea ecx, [ebx]
+				mov [edx].item.pszText, ecx
+			.elseif [edx].item.iSubItem == 1
+				invoke wsprintfW, addr [edx].item.pszText, offset d_fmt, [ebx].filesize 
+			.endif
+		.endif
+
+	assume ebx:nothing
+	assume edx:nothing
+
+	ret
+handleListViewNotify endp
 
 
 
 WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM 
-	LOCAL ps:PAINTSTRUCT 
-
+	local ps:PAINTSTRUCT 
+	local wmId:WORD
 	.if uMsg == WM_DESTROY
 		invoke PostQuitMessage,NULL 
+
+	.elseif uMsg == WM_COMMAND
+		mov eax, DWORD PTR [wParam]
+		and eax, 0ffffh
+		; mov wmId, ax
+		.if eax == IDC_OPENDIRBTN
+			invoke SelectDir, hWnd
+			invoke FindFile
+		.elseif eax == IDC_PLAYBTN
+			
+		.elseif eax == IDC_STOPBTN
+			
+		.endif
+
 	.elseif uMsg == WM_PAINT
 		invoke BeginPaint, hWnd, ADDR ps 
 		invoke EndPaint, hWnd, ADDR ps 
+	.elseif uMsg == WM_NOTIFY
+		mov eax, DWORD PTR [wParam]
+		and eax, 0ffffh
+		.if eax == IDC_LISTVIEW
+			 invoke handleListViewNotify, lParam;bug待解决
+		.endif
 
 	; 创建控件
 	.elseif uMsg == WM_CREATE
@@ -315,7 +524,7 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
 	.elseif uMsg == WM_SIZE
 		invoke ReSizeWindowControl, hWnd
 	.else
-		invoke DefWindowProc,hWnd,uMsg,wParam,lParam 
+		invoke DefWindowProc, hWnd, uMsg, wParam, lParam 
 	.endif
 	ret
 WndProc endp
